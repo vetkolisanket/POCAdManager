dad1a5f5edbd6bcb1844954db6a0061f02b3a5a7
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-10-07 16:58:10 +0530

    Hid native add button from MA

---
+++ app/src/main/java/com/sanket/pocadmanager/NativeAdActivity.kt
@@ -0,0 +1,11 @@
+package com.sanket.pocadmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NativeAdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_native_ad)
+    }
+}
